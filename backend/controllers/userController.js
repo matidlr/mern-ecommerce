d35f88e58a89924e02621e6cc4aed83f541982d9
matidlr <argentinavende.sa@gmail.com>
2024-07-03 17:52:59 -0300

    create user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,36 @@
 import User from '../models/userModel.js'
 import asyncHandler from '../middlewares/asyncHandler.js'
-
+import bcryptjs from 'bcryptjs'
 
 const createUser = asyncHandler(async (req, res) => {
-    res.send("Hello")
+    const { username, email, password } = req.body;
+    if (!username || !email || !password){
+        throw new Error("Please fill out all the inputs.")
+    }
+
+    const userExist = await User.findOne({ email });
+    if(userExist) res.status(400).send('User already exists');
+
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password, salt)
+
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    try {
+        await newUser.save();
+
+        res
+           .status(201)
+           .json({
+              _id: newUser._id,
+              username: newUser.username,
+              email: newUser.email,
+              isAdmin: newUser.isAdmin,
+            });
+    } catch (error) {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 });
 
 export {
