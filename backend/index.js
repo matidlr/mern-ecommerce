021be81e167d5566b9c9d845a0e2221b0d50a437
matidlr <argentinavende.sa@gmail.com>
2024-07-03 16:28:20 -0300

    create user model

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,5 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.get("/test", (req, res) => {
-    res.send("Hello World");
-})
-
+app.use('/api/users', userRoutes);
 app.listen(port, ()=> console.log(`Server running on port: ${port}`));
