fd8a88b9a7e9232847f2a996be1a9a6021fe4f2a
matidlr <argentinavende.sa@gmail.com>
2024-07-04 14:23:18 -0300

    navigation sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-
+import {Outlet} from 'react-router-dom'
+import Navigation from './pages/Auth/Navigation'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
 
   return (
-    <div className="font-bold text-red-400">
-    hello
-    </div>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className='py-3'>
+        <Outlet />
+      </main>
+    </>
   )
 }
 
