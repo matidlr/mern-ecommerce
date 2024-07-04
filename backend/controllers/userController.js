5a1b82d51078e19cf1d5fcc32c0a816a1d82d078
matidlr <argentinavende.sa@gmail.com>
2024-07-03 23:58:31 -0300

    create getallusers route, controller and auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,8 +66,14 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(200),json({ message: "Logged out successfully" });
  });
 
+ const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+ })
+
 export {
     createUser,
     loginUser,
-    logout
+    logout,
+    getAllUsers,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {createUser, loginUser, logout} from '../controllers/userController.js'
+import {createUser, loginUser, logout, getAllUsers} from '../controllers/userController.js'
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
 const router = express.Router()
 
-router.route('/').post(createUser);
+router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post('/logout', logout);
 
