7cbf74d3400377988526cd7e1ae63e0a12b09f46
matidlr <argentinavende.sa@gmail.com>
2024-07-07 19:24:21 -0300

    fetch all products route and controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import formidable from 'express-formidable';
 const router = express.Router()
 
-import { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById } from "../controllers/productController.js";
+import { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById, fetchAllProducts } from "../controllers/productController.js";
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 import checkId from "../middlewares/checkid.js";
 
 router.route('/')
            .get(fetchProducts)
            .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route('/allproducts').get(fetchAllProducts)
 
 router.route('/:id')
          .get(fetchProductById)
