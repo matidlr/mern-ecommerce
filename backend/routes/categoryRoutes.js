c40c18df351653d1fefaea7eebb479e7f79844ba
matidlr <argentinavende.sa@gmail.com>
2024-07-07 01:00:09 -0300

    update category route and controller

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { createCategory } from '../controllers/categoryController.js';
+import { createCategory, updateCategory } from '../controllers/categoryController.js';
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 router.route('/').post(authenticate, authorizeAdmin, createCategory);
+router.route('/:categoryId').put(authenticate, authorizeAdmin, updateCategory)
 
 export default router;
