011af29474803d27809e0b3a58b6d6e3b01eda4c
matidlr <argentinavende.sa@gmail.com>
2024-07-07 18:55:40 -0300

    update products details routes and controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,11 @@ import express from "express";
 import formidable from 'express-formidable';
 const router = express.Router()
 
-import { addProduct } from "../controllers/productController.js";
+import { addProduct, updateProductDetails } from "../controllers/productController.js";
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 import checkId from "../middlewares/checkid.js";
 
 router.route('/').post(authenticate, authorizeAdmin, formidable(), addProduct);
+router.route('/:id').put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
 
 export default router;
