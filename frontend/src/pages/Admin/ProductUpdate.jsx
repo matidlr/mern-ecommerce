5b193767c2a5a4f0be0f63c373a29e0236737548
matidlr <argentinavende.sa@gmail.com>
2024-07-08 22:10:09 -0300

    product update page

--- frontend/src/pages/Admin/ProductUpdate.jsx
+++ frontend/src/pages/Admin/ProductUpdate.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import AdminMenu from "./AdminMenu";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   useUpdateProductMutation,
@@ -53,8 +53,222 @@ const ProductUpdate = () => {
           setImage(productData.image);
         }
       }, [productData]);
-      
-    return <div></div>;
+
+      const uploadFileHandler = async (e) => {
+        const formData = new FormData();
+        formData.append("image", e.target.files[0]);
+        try {
+          const res = await uploadProductImage(formData).unwrap();
+          toast.success("Item added successfully", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+          setImage(res.image);
+        } catch (err) {
+          toast.success("Item added successfully", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+        }
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const formData = new FormData();
+          formData.append("image", image);
+          formData.append("name", name);
+          formData.append("description", description);
+          formData.append("price", price);
+          formData.append("category", category);
+          formData.append("quantity", quantity);
+          formData.append("brand", brand);
+          formData.append("countInStock", stock);
+    
+          // Update product using the RTK Query mutation
+          const data = await updateProduct({ productId: params._id, formData });
+    
+          if (data?.error) {
+            toast.error(data.error, {
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 2000,
+            });
+          } else {
+            toast.success(`Product successfully updated`, {
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 2000,
+            });
+            navigate("/admin/allproductslist");
+          }
+        } catch (err) {
+          console.log(err);
+          toast.error("Product update failed. Try again.", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+        }
+      };
+
+      const handleDelete = async () => {
+        try {
+          let answer = window.confirm(
+            "Are you sure you want to delete this product?"
+          );
+          if (!answer) return;
+    
+          const { data } = await deleteProduct(params._id);
+          toast.success(`"${data.name}" is deleted`, {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+          navigate("/admin/allproductslist");
+        } catch (err) {
+          console.log(err);
+          toast.error("Delete failed. Try again.", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+          });
+        }
+      };
+
+
+
+    return (
+        <>
+    <div className="container xl:mx-[9rem] sm:mx-[0]">
+      <div className="flex flex-col md:flex-row">
+          <AdminMenu />
+        <div className="md:w-3/4 p-3">
+            <div className="h-12">Update / Delete Product</div>
+
+            {image && (
+    <div className="text-center">
+      <img
+        src={image}
+        alt="product"
+        className="block mx-auto max-h-[200px]"
+      />
+    </div>
+  )}
+
+  <div className="mb-3">
+    <label className="border text-white px-4 block w-full text-center rounded-lf cursor-pointer font-bold py-11">
+        {image ? image.name : "Upload Image"}
+
+        <input 
+            type="file" 
+            name='image' 
+            accept="image/*" 
+            onChange={uploadFileHandler}
+            className={!image ? "hidden" : "text-white"} 
+            />
+    </label>
+  </div>
+
+     <div className="p-3">
+        <div className="md:flex">
+            <div className="one">
+                <label htmlFor="name">Name</label> <br />
+                <input 
+                    type="text" 
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    />
+            </div>
+            <div className="two md:ml-10">
+                <label htmlFor="name block">Price</label> <br />
+                <input 
+                    type="number"                            
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                    value={price}
+                    onChange={e => setPrice(e.target.value)}
+                    />
+            </div>
+        </div>
+     </div>
+
+     
+     <div className="p-3">
+        <div className="md:flex">
+            <div className="one">
+                <label htmlFor="name block">Quantity</label> <br />
+                <input 
+                    type="number" 
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                    value={quantity}
+                    onChange={e => setQuantity(e.target.value)}
+                    />
+            </div>
+            <div className="two md:ml-10">
+                <label htmlFor="name block">Brand</label> <br />
+                <input 
+                    type="text"                            
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                    value={brand}
+                    onChange={e => setBrand(e.target.value)}
+                    />
+            </div>
+        </div>
+
+        <label htmlFor="" className="my-5">
+      Description
+    </label>
+    <textarea
+      type="text"
+      className="p-2 mb-3 bg-[#101011] border rounded-lg w-[95%] text-white"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+    ></textarea>
+
+        <div className="md:flex">
+            <div>
+                    <label htmlFor="name block">Count In Stock</label> <br />
+                <input
+                type="text"
+                className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                />
+            </div>
+
+            <div className="md:ml-10">
+                <label htmlFor="">Category</label> <br />
+                <select 
+                     placeholder="Choose Category"
+                     className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                     onChange={(e) => setCategory(e.target.value)}
+                     >
+                      {categories?.map((c) => (
+                        <option key={c._id} value={c._id} >
+                            {c.name}
+                        </option>
+                      ))}      
+                </select>
+            </div>
+        </div>
+         
+        <div className="">
+                <button
+                  onClick={handleSubmit}
+                  className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-green-600 mr-6"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-pink-600"
+                >
+                  Delete
+                </button>
+              </div>
+
+     </div>
+   </div>
+</div>
+</div>
+</>
+);
 };
 
 export default ProductUpdate;
