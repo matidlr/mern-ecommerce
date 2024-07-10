f0935750b3de0a82140c0045d0d65cb1491d2f39
matidlr <argentinavende.sa@gmail.com>
2024-07-10 13:26:08 -0300

    product details and ratings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,7 @@ import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 import AllProducts from './pages/Admin/AllProducts.jsx';
 import Home from './pages/Home.jsx';
 import Favorites from './pages/Products/Favorites.jsx';
+import ProductDetails from './pages/Products/ProductDetails.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
         <Route path="/register" element={<Register />}/>
         <Route index={true} path='/' element={<Home />} />
         <Route path='/favorite' element={<Favorites />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
 
         {/* Registered users */}
       <Route path='' element={<PrivateRoute />}>
